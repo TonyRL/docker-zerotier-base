3078082ed6a2940086e7f04323ff34c9865a3ba2
Tony <TonyRL@users.noreply.github.com>
2020-02-28 22:22:56 +0800

    Add Dockerfile

---
+++ main.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
+
+if [ ! -e /dev/net/tun ]; then
+	echo 'FATAL: cannot start ZeroTier One in container: /dev/net/tun not present.'
+	exit 1
+fi
+
+zerotier-one & export APP_PID=$!
+sleep 5
+
+for ID in $(echo $NETWORK_ID | tr "," "\n")
+do
+    zerotier-cli join $ID
+done
+
+wait $APP_PID
